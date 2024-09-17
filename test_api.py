054db989a53201c128fcd1bbaa273897ddaae828
yuliia.salaman <yuliia.salaman@gmail.com>
2024-09-17 16:53:42 -0700

    Describe the changes you madeinitial

--- test_api.py
+++ test_api.py
@@ -29,3 +29,58 @@ def test_get_all_figures():
     assert_that(response_dict).is_instance_of(list).is_not_empty()
     assert_that(response_dict).is_not_empty() #check that API returns content
 
+def test_get_figure_by_id():
+    # Send the GET request
+    response = requests.get(BASE_URL)
+
+    # Convert response to JSON
+    figures = response.json()
+
+    figure_ids = [figure['id'] for figure in figures]
+
+    # Construct the URL with the specific ID
+    url = f"{BASE_URL}/{FIGURES_URL}/{figure_ids[0]}"
+
+    response = requests.get(url)
+
+    # Check status code
+    assert_that(response.status_code).is_equal_to(200)
+
+# # Test case to retrieve the Bearer token
+# @pytest.mark.api
+# def test_get_bearer_token():
+#     token = get_bearer_token(LOGIN_URL)
+#
+#     # Ensure that a token is returned
+#     assert token is not None
+#     assert isinstance(token, str)
+#
+#     # Print the token for debugging (optional)
+#     print(f"Bearer Token: {token}")
+#
+#     return token
+#
+#
+# # Test case to use the Bearer token and check the user details
+# @pytest.mark.api
+# def test_get_user_details():
+#     # Get the Bearer token from the previous test
+#     token = get_bearer_token(LOGIN_URL)
+#
+#     # Set the headers with the Bearer token
+#     headers = {
+#         'Authorization': f'Bearer {token}',
+#         'accept': 'application/json'
+#     }
+#
+#     # Send GET request to retrieve user details
+#     response = requests.get(USER_URL, headers=headers)
+#
+#     # Assertions to ensure the request was successful
+#     assert response.status_code == 200
+#     assert response.headers['Content-Type'] == 'application/json'
+#
+#     # Optionally, you can validate specific data from the response
+#     user_data = response.json()
+#     assert 'email' in user_data
+#     assert user_data['email'] == "online.store.authority@gmail.com"
